3f3b46905205c427f9fc2f5c3cc27c12fe5ace7c
Godswill Sekav Apya <godswillapya@gmail.com>
2025-08-16 23:56:38 +0100

    Refactor code style for consistency in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { PORT } from './config/env.js'
+import { PORT } from "./config/env.js";
 
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
@@ -12,23 +12,25 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended:false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/api/v1/auth', authRouter);
-app.use('/api/v1/users', userRouter);
-app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 app.use(errorMiddleware);
 
-app.get('/', (req, res) => {
-    res.send('<h1>Welcome to the Subscription Tracker API</h1>');
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the Subscription Tracker API</h1>");
 });
 
 app.listen(PORT, async () => {
-    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+  console.log(
+    `Subscription Tracker API is running on http://localhost:${PORT}`
+  );
 
-    await connectToDatabase();
+  await connectToDatabase();
 });
 
 export default app;
